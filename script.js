ef2cb12ce3e53c41d01547fe83b4903dbab23573
ms-chatterbox <itsmechatterboxjd@outlook.com>
2024-04-18 14:50:59 +0530

    Added the files

---
+++ script.js
@@ -0,0 +1,19 @@
+var txtNumber = document.getElementById('txtNumber');
+var baseDDL = document.getElementById('base');
+var convertBtn = document.getElementById('btnConvert');
+var spanEl = document.getElementById('convertedValue');
+
+convertBtn.addEventListener('click',function(){
+    let num = +txtNumber.value;
+    let base = +baseDDL.value;
+    if(isNaN(num)){
+        spanEl.textContent = 'Please enter a valid number';
+        spanEl.style.color = '#EF5350';
+    }
+    else{
+        let convertedValue = num.toString(base);
+        spanEl.textContent = convertedValue.toUpperCase();
+        spanEl.style.color = '#81C784';
+        spanEl.style.fontWeight = 'bold';
+    }     
+})
